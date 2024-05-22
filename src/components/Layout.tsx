e8bc2d7d1b6e59e334a0f5fcbbcf4e09ddea56b7
Hajar Hamza <hajar.hamza1@gmail.com>
2024-05-22 20:48:19 +0200

    wip fix layout - layout - header - footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,16 @@
-import { Link, Outlet } from "react-router-dom";
-import AuthStatus from "./AuthStatus";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <div>
-      Header with links and login status
-      <Link to="/">Home</Link>
-      <Link to="/category">Category</Link>
-      <Link to="/bookmarked">Bookmarked</Link>
-      <Link to="/movies/:id">Single Movie</Link>
-      <AuthStatus />
-      <Outlet />
-      Footer
-    </div>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 };
 
